419d0f20e350af8c4574360bd01ed2487a0be95b
Diana <ivdiana14500@gmail.com>
2023-11-09 20:12:02 +0300

    bagfix: fix a map errors  and add mini-contacts in map

--- src/components/BlockContacts/Contacts.tsx
+++ src/components/BlockContacts/Contacts.tsx
@@ -1,13 +1,32 @@
 import MapBlock from "../Map/Map";
+import SocialLinks from "../SocialLinks/SocialLinks";
 import TitleSection from "../Title/Title";
 import css from "./Contacts.module.scss";
-
+import sprite from "../../assets/sprite.svg";
 
 const Contacts = () => {
   return (
     <section className={css.contacts} id="contactsBlock">
-       <TitleSection title="Контакты"/>
-       <MapBlock />
+      <TitleSection title="Контакты" />
+      <MapBlock />
+      <div className={css.info}>
+        <div className={css.location}>
+          <svg width="17" height="17" viewBox="0 0 17 17">
+            <use xlinkHref={`${sprite}#location`}></use>
+          </svg>
+          <p>Адрес, город, улица, дом</p>
+        </div>
+
+        <div className={css.phone}>
+          <svg width="14" height="14" viewBox="0 0 14 14">
+            <use xlinkHref={`${sprite}#phone`}></use>
+          </svg>
+          <p>+7 (495) 788-77-50</p>
+        </div>
+
+        <p>Мы в социальных сетях:</p>
+        <SocialLinks />
+      </div>
     </section>
   );
 };
