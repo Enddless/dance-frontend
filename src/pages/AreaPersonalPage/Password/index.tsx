4a571b55e1feee17076b185bb6610a39cbb91662
Diana <ivdiana14500@gmail.com>
2023-11-26 12:56:21 +0300

    feat: update change password user component

--- src/pages/AreaPersonalPage/Password/index.tsx
+++ src/pages/AreaPersonalPage/Password/index.tsx
@@ -1,7 +1,38 @@
+import Button from "../../../components/Button/Button";
 import css from "./styles.module.scss";
 
 const ChangePassword = () => {
-  return <div className={css.container}>ChangePasswordPage</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.form}>
+        <label htmlFor="currentPassword">Текущий пароль</label>
+        <input
+          type="password"
+          name="currentPassword"
+          id="currentPassword"
+          className={css.checkbox}
+        />
+
+        <label htmlFor="newPassword">Новый пароль</label>
+        <input
+          type="password"
+          name="newPassword"
+          id="newPassword"
+          className={css.checkbox}
+        />
+
+        <label htmlFor="doubleNewPassword">Подтвердите пароль</label>
+        <input
+          type="password"
+          name="doubleNewPassword"
+          id="doubleNewPassword"
+          className={css.checkbox}
+        />
+
+        <Button text="Сменить пароль" cls="btn-enter" />
+      </div>
+    </div>
+  );
 };
 
 export default ChangePassword;
