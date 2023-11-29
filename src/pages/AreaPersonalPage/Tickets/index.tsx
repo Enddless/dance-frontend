e2679ad17048e5379bf4c5b5c499eae6ad20a94d
Olga Vasilevskaya <olgavasilevskaya@MacBook-Air-Olga.local>
2023-11-29 21:58:35 +0300

    add tickets page

--- src/pages/AreaPersonalPage/Tickets/index.tsx
+++ src/pages/AreaPersonalPage/Tickets/index.tsx
@@ -1,12 +1,90 @@
-
-
 import css from "./styles.module.scss";
 
+const titles = [
+  "Абонемент",
+  "Дата покупки",
+  "Дата начала",
+  "Дата окончания",
+  "Посетил занятий",
+  "Состояние",
+];
+
+interface ITicketsData {
+  id: number;
+  dateOfPurchase: string;
+  startDate: string;
+  endDate: string;
+  usedClasses: number;
+  totalClasses: number;
+  status: string;
+}
+
+const ticketsData: ITicketsData[] = [
+  {
+    id: 1,
+    dateOfPurchase: "01.10.23",
+    startDate: "02.10.23",
+    endDate: "02.11.23",
+    usedClasses: 5,
+    totalClasses: 12,
+    status: "Активный",
+  },
+  {
+    id: 2,
+    dateOfPurchase: "01.10.23",
+    startDate: "02.10.23",
+    endDate: "02.11.23",
+    usedClasses: 5,
+    totalClasses: 12,
+    status: "Неактивный",
+  },
+  {
+    id: 3,
+    dateOfPurchase: "01.10.23",
+    startDate: "02.10.23",
+    endDate: "02.11.23",
+    usedClasses: 5,
+    totalClasses: 12,
+    status: "Завершенный",
+  },
+];
+
 const Tickets = () => {
-  
   return (
     <div className={css.container}>
-      TicketsPage
+      <table className={css.ticketsTable}>
+        <thead>
+          <tr className={css.tableTitle}>
+            {titles.map((item) => (
+              <th key={item}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {ticketsData.map(
+            ({
+              id,
+              dateOfPurchase,
+              startDate,
+              endDate,
+              usedClasses,
+              totalClasses,
+              status,
+            }) => (
+              <tr className={css.tableRow} key={id}>
+                <td>{id}</td>
+                <td>{dateOfPurchase}</td>
+                <td>{startDate}</td>
+                <td>{endDate}</td>
+                <td>
+                  {usedClasses}/{totalClasses}
+                </td>
+                <td>{status}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
