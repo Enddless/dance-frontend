8025eaf0619ec049b6031b8bbf9afee6e0134e82
Diana <ivdiana14500@gmail.com>
2023-12-12 15:03:21 +0300

    feat: delete async api refresh

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,11 +1,13 @@
 import axios, { AxiosInstance } from "axios";
-import { deleteToken, getToken } from "./token";
-import { updateToken } from "./thunk/auth";
+import { getToken } from "./token";
+
 
 export const API_URL = "http://localhost:8585";
 export const REQUEST_TIMEOUT = 5000;
 
+
 export const createAPI = (): AxiosInstance => {
+  
   const api = axios.create({
     baseURL: API_URL,
     timeout: REQUEST_TIMEOUT,
@@ -15,36 +17,20 @@ export const createAPI = (): AxiosInstance => {
       "Content-Type": "application/json",
     },
   });
+  // Перехватчик для обработки исходящих запросов
+  api.interceptors.request.use((config) => {
+    const token = getToken();
 
-  api.interceptors.request.use(
-    (config) => {
-      const token = getToken();
-
-      if (token && config.headers) {
-        // config.headers["x-token"] = token;
-        config.headers.Authorization = `Bearer ${token.token}`;
-      }
-
-      return config;
-    },
-    async (error) => {
-      const originalRequest = error.config;
-      if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        try {
-          await updateToken();
-          const token = getToken();
-          originalRequest.headers.Authorization = `Bearer ${token.token}`;
-          return api(originalRequest);
-        } catch (error) {
-          deleteToken();
-          window.location.reload();
-        }
-      }
-      return Promise.reject(error);
+    if (token && config.headers) {
+      // config.headers["x-token"] = token;
+      config.headers.Authorization = `Bearer ${token.token}`;
     }
 
-  );
+    return config;
+  });
+
+  
+
 
   return api;
 };
