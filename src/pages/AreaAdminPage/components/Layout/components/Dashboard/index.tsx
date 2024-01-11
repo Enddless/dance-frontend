dd8f28a1f08d9956a482a5ce4ac49a9dfa53dab8
Diana <ivdiana14500@gmail.com>
2024-01-11 17:45:09 +0300

    feat: correction dashboard

--- src/pages/AreaAdminPage/components/Layout/components/Dashboard/index.tsx
+++ src/pages/AreaAdminPage/components/Layout/components/Dashboard/index.tsx
@@ -1,7 +1,10 @@
+import Dashboard from "../../../../../MainPage/components/Dashboard/Dashboard";
 import css from "./styles.module.scss";
 
 const DashboardSettings = () => {
-  return <div className={css.container}>Dashboard settings</div>;
+  return <div className={css.container}>
+    <Dashboard />
+  </div>;
 };
 
 export default DashboardSettings;
