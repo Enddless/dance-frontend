1adac86248891d6a503ae280b80096e79d70cf4a
Diana <ivdiana14500@gmail.com>
2024-01-12 11:04:58 +0300

    feat: correction personal

--- src/pages/AreaAdminPage/components/Layout/components/Personal/index.tsx
+++ src/pages/AreaAdminPage/components/Layout/components/Personal/index.tsx
@@ -1,28 +1,34 @@
 import { StaffInfo } from "../../../../../../mocks/mocks";
 import css from "./styles.module.scss";
 import ControlButton from "../../../../../../components/controls-button";
+import Button from "../../../../../../components/Button/Button";
 const Personal = () => {
   return (
-    <div className={css.container}>
-      {StaffInfo.map(({ name, img, description, id }) => (
-        <div key={id} className={css.staffContainer}>
-          <div className={css.controlGroup}>
-            <ControlButton id="edit" />
-            <ControlButton id="delete" />
-          </div>
-          <div className={css.staffInfo}>
-            <div className={css.avatar}>
-              <img src={img} alt="staffPhoto" />
+    <>
+      <div className={css.container}>
+        {StaffInfo.map(({ name, img, description, id }) => (
+          <div key={id} className={css.staffContainer}>
+            <div className={css.controlGroup}>
+              <ControlButton id="edit" />
+              <ControlButton id="delete" />
             </div>
+            <div className={css.staffInfo}>
+              <div className={css.avatar}>
+                <img src={img} alt="staffPhoto" />
+              </div>
 
-            <div className={css.staffDescr}>
-              <h6>{name}</h6>
-              <p>{description}</p>
+              <div className={css.staffDescr}>
+                <h6>{name}</h6>
+                <p>{description}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div className={css.add}>
+        <Button text="Добавить" cls="add" />
+      </div>
+    </>
   );
 };
 
