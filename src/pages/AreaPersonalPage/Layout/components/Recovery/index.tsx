e5b1b15e4681350a63891ec0f44cb96605bc4ec2
Diana <ivdiana14500@gmail.com>
2024-01-22 09:36:19 +0300

    feat: correct record

--- src/pages/AreaPersonalPage/Layout/components/Recovery/index.tsx
+++ src/pages/AreaPersonalPage/Layout/components/Recovery/index.tsx
@@ -5,8 +5,38 @@ import Button from "../../../../../components/Button/Button";
 import SelectFilter from "../../../../../components/Select";
 import { halls, lessons, times } from "../../../../../mocks/mocks";
 
+const titles = ["Дата", "Время", "Занятие"];
+
+interface ILessonsData {
+  id: number;
+  startDate: string;
+  time: string;
+  lesson: string;
+}
+
+const lessonsData: ILessonsData[] = [
+  {
+    id: 1,
+    startDate: "02.10.23",
+    time: "09:00",
+    lesson: "Занятие 1",
+  },
+  {
+    id: 2,
+    startDate: "02.10.23",
+    time: "12:00",
+    lesson: "Занятие 2",
+  },
+  {
+    id: 3,
+    startDate: "02.10.23",
+    time: "18:00",
+    lesson: "Занятие 3",
+  },
+];
+
 const OnlineRecovery = () => {
-  const filtersTitle = ["Выбор зала", "Выбор занятия", "Выбор времени"];
+  const filtersTitle = ["Выбор занятия", "Выбор времени"];
   const record = true;
   return (
     <div className={css.wrapper}>
@@ -29,6 +59,29 @@ const OnlineRecovery = () => {
           </div>
 
           <Button text="Записаться" cls="btn-rec" />
+          <div className={css.content}>
+            <h5 className={css.title}>Текущие записи</h5>
+            <table className={css.lessonsTable}>
+              <thead>
+                <tr className={css.lessonsTitle}>
+                  {titles.map((item) => (
+                    <th key={item}>{item}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {lessonsData.map(({ id, startDate, time, lesson }) => {
+                  return (
+                    <tr className={css.tableRow} key={id}>
+                      <td>{startDate}</td>
+                      <td>{time}</td>
+                      <td>{lesson}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </>
       ) : (
         <EmptyTemplate>
