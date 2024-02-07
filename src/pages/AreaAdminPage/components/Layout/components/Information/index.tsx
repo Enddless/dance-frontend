556da6d597b62a99c9c99b13a043b19e00d1232b
Diana <ivdiana14500@gmail.com>
2024-02-07 11:53:43 +0300

    feat: add information

--- src/pages/AreaAdminPage/components/Layout/components/Information/index.tsx
+++ src/pages/AreaAdminPage/components/Layout/components/Information/index.tsx
@@ -1,48 +1,31 @@
-import { useState } from "react";
 import ControlButton from "../../../../../../components/controls-button";
 import css from "./styles.module.scss";
 
 const LegalInformation = () => {
-  const data =
-    "Lorem ipsum dolor sit amet consectetur. Urna cras nibh nullam ornare facilisis euismod massa venenatis vel. Tempor ut imperdiet scelerisque dictumst. Pellentesque accumsan adipiscing quis pulvinar fusce pharetra. Proin tellus malesuada nisl nibh sed. Aliquet ultricies elementum aliquam viverra parturient luctus. Ultricies velit nullam eu non hac tellus congue. Volutpat at nisi urna augue risus in. Id amet purus nunc eget. Ut ut mauris vitae hac id. Nisi tempor turpis non semper adipiscing commodo.Placerat consequat euismod pulvinar in viverra sed habitasse mattis risus. Aliquam dignissim at urna eget consectetur nullam dictum. Feugiat sit sed sit id amet cursus aliquam. Sed amet lorem at feugiat tortor sed odio sit. Felis curabitur amet nunc iaculis quis et a nisl. Vestibulum sagittis elit et tristique eget malesuada ante. In elementum vel integer bibendum. Donec tristique amet elit congue non vulputate. At orci platea turpis placerat. Iaculis posuere nisl vel commodo mauris nam risus quis nunc. Hendrerit ultricies tempor tempor integer. Vitae duis integer nisi blandit turpis sit. Sit massa bibendum lorem risus proin amet. Ullamcorper integer sed lectus dolor.";
-  const [description, setDescription] = useState(data);
-  const [isActive, setIsActive] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const deleteData = () => {
-    setDescription("");
-  };
   return (
-    <div className={css.container}>
-      <div className={css.controlGroup}>
-        <ControlButton
-          id="edit"
-          onClick={() => {
-            setIsEditing(true);
-            setIsActive(true);
-          }}
-          isActive={isActive}
-        />
-        <ControlButton id="delete" onClick={deleteData} />
+    <>
+      <h2>Юридическая информация</h2>
+      <div className={css.container}>
+        <div className={css.document}>
+          <div className={css.file}>
+            Условия использования и Политика конфиденциальности
+          </div>
+          <div className={css.controlGroup}>
+            <ControlButton id="delete" />
+            <ControlButton id="delete" />
+          </div>
+        </div>
+        <div className={css.document}>
+          <div className={css.file}>
+            Условия использования и Политика конфиденциальности Условия использования и Политика конфиденциальности
+          </div>
+          <div className={css.controlGroup}>
+            <ControlButton id="delete" />
+            <ControlButton id="delete" />
+          </div>
+        </div>
       </div>
-
-      {isEditing ? (
-        <textarea
-          rows={25}
-          value={description}
-          onBlur={() => {
-            setIsEditing(false);
-            setIsActive(false);
-          }}
-          onChange={(e) => setDescription(e.target.value)}
-          className={css.editText}
-        />
-      ) : (
-        <>
-          <p style={{ whiteSpace: "pre-line" }}>{description}</p>
-        </>
-      )}
-    </div>
+    </>
   );
 };
 
