06298154607e64401aa5a29332d7ec15ce0f5313
Diana <ivdiana14500@gmail.com>
2024-03-14 10:25:36 +0300

    feat: add env config

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,9 +2,8 @@ import axios, { AxiosInstance } from "axios";
 import { addToken, deleteToken, getToken } from "./token";
 import { APIRoute, AppRoute } from "../const/route";
 
-export const API_URL = "http://localhost:8585";
-// const baseURL = process.env.NODE_ENV === 'development' ? '/' : 'https://example.co.kr/';
-export const REQUEST_TIMEOUT = 5000;
+export const API_URL = import.meta.env.VITE_API_URL;
+export const REQUEST_TIMEOUT = import.meta.env.VITE_REQUEST_TIMEOUT;
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -57,23 +56,6 @@ export const createAPI = (): AxiosInstance => {
           }
           return Promise.reject(error);
         }
-
-
-        // await api.post(APIRoute.UpdateToken)
-        //   .then((response) => {
-        //     originalRequest._retry = true;
-        //     const token = response.data.token;
-        //     originalRequest.headers.Authorization = `Bearer ${token}`;
-        //     addToken({ token });
-        //     return api(originalRequest);
-        //   })
-        //   .catch((error) => {
-        //     if (error.response.status === 500) {
-        //       deleteToken();
-        //       window.location.href = `${AppRoute.Root}`
-        //     }
-        //   });
-
       }
       return Promise.reject(error);
     }
