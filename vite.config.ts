89ae1c951cd973dcc75d4fda5d72edc53ee9ccd5
Diana <ivdiana14500@gmail.com>
2024-10-03 13:04:09 +0300

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,23 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
+  base: '/studia/',
   resolve: {
     alias: {
       public: path.resolve(__dirname, 'public'),
       '@': path.resolve(__dirname, 'src'),
       '@fonts': path.resolve(__dirname, 'public/fonts')
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString(); // Разделяет модули по пакетам
+          }
+        }
+      }
     }
   }
 });
